a862b6030f5a6308d74c93e7e70935a91cfdef61
Damian Sędrowski <damian.sedrowski@gmail.com>
2018-06-01 09:56:52 +0200

    wykorzystywanie i wstrzykiwanie zewnetrznych bibliotek

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -52,8 +52,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLogin)
         {
-            throw new Exception("Computer says no!");
-
             var userFromRepo = await _repo.Login(userForLogin.Username.ToLower(), userForLogin.Password);
 
             if (userFromRepo == null)

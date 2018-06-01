7db47ae371b0f0f2074982a8eccb9b99b7e2eb3a
Damian Sędrowski <damian.sedrowski@gmail.com>
2018-06-01 15:26:06 +0200

    deszyfrowanie tokenów jwt

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -5,11 +5,14 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { IfObservable } from 'rxjs/observable/IfObservable';
 import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
     baseUrl = 'http://localhost:5000/api/auth/';
     userToken: any;
+    decodedToken: any;
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(private http: Http) { }
 
@@ -19,7 +22,10 @@ export class AuthService {
 
             if (user) {
                 localStorage.setItem('token', user.tokenString);
+                this.decodedToken = this.jwtHelper.decodeToken(user.tokenString);
                 this.userToken = user.tokenString;
+
+                console.log(this.decodedToken);
             }
         }).catch(this.handleError);
     }
@@ -28,13 +34,17 @@ export class AuthService {
         return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
     }
 
+    loggedIn() {
+        return tokenNotExpired('token');
+    }
+
     private requestOptions() {
         const headers = new Headers({'Content-type': 'application/json'});
 
         return new RequestOptions({headers: headers});
     }
 
-    private handleError(error: any){
+    private handleError(error: any) {
         const applicationError = error.headers.get('Application-Error');
 
         if (applicationError) {
